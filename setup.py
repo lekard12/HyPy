c678d7e6efc3f58b09fdf5b56549dc88315d0247
lekard12 <yejunandterryyt@gmail.com>
2025-09-15 16:03:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name="HyPy",
     version="0.0.1",
     description="A collection of graph, sorting, and search algorithms",
-    author="Your Name",
-    author_email="you@example.com",
-    packages=find_packages(),
     python_requires=">=3.7",
     install_requires=[],
 )
